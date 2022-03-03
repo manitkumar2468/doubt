aad840fd14119e0b3a80fc91151ce71f59ae76aa
manitkumar2468 <manitkumar1998@gmail.com>
2022-03-03 22:37:14 +0530

    Add files via upload

---
+++ doubt/index.js
@@ -0,0 +1,35 @@
+
+// function show(){
+// // when click on button change the 
+// // background color by changing the 
+// // body class
+
+// var body = document.body;
+
+// var theme = document.getElementById("button");
+
+// if(body.classList.contains("light")){
+//     body.classList.replace("light" , "dark");
+//     theme.innerText ="Light Mode" ;
+// }
+
+// else {
+//     body.classList.replace("dark","light");
+//     theme.innerText ="Dark Mode" ;
+// }
+// // console.log(body.classList);
+
+// // body.classList.remove("light");
+// // console.log(body.classList);
+// // body.classList.add("dark");
+// // console.log(body.classList);
+
+// }
+
+function close(){
+
+    alert("hello");
+    // var pop_hide = document.getElementsByClassName("popup");
+    // alert("hello");
+    // pop_hide[0].classList.add("hide")
+}
